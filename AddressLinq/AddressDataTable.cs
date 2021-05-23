0cbefa8a8a129e41e29e912005664b550728ce07
PriyanshuSingh-5 <singhprin05@gmail.com>
2021-05-23 14:41:18 +0530

    [Priyanshu] added create table

---
+++ AddressLinq/AddressDataTable.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Data;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressLinq
+{
+    class AddressDataTable
+    {
+       
+        // UC1 Created new address book table
+        
+        public static DataTable addressBookTable = new DataTable();
+    }
+}
